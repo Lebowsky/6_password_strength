1ab3bc0b6625aa0e050ba83a97489c74a4f5abeb
Andrey Lebowski <andrey.lebowski@gmail.com>
2018-01-03 21:33:05 +0300

    increase readability

--- password_strength.py
+++ password_strength.py
@@ -2,37 +2,44 @@ import getpass
 import re
 
 
+def get_check_list():
+    check_list = {}
+
+    check_list['min_length'] = 6
+    check_list['middle_length'] = 10
+    check_list['has_lowercase'] = re.search(r'[a-z]', password)
+    check_list['has_uppercase'] = re.search(r'[A-Z]', password)
+    check_list['has_numbers'] = re.search(r'\d', password)
+    check_list['has_symbols'] = re.search(r'[!@#$%^&*()_+]', password)
+    check_list['has_date'] = re.search(r'\d{2,4}[.-]\d{2}[.-]\d{2,4}', password)
+    check_list['has_phone_number'] = re.search(r'[7-8]\d{10}', password)
+    return check_list
+
+
 def get_password_strength(password):
     password_strength = 0
 
-    min_length = 6
-    middle_length = 10
-    has_lowercase = re.search(r'[a-z]', password)
-    has_uppercase = re.search(r'[A-Z]', password)
-    has_numbers = re.search(r'\d', password)
-    has_symbols = re.search(r'[!@#$%^&*()_+]', password)
-    has_date = re.search(r'\d{2,4}[.-]\d{2}[.-]\d{2,4}', password)
-    has_phone_number = re.search(r'[7-8]\d{10}', password)
+    check_list = get_check_list()
 
-    if len(password) >= min_length:
+    if len(password) >= check_list['min_length']:
         password_strength += 1
 
-    if len(password) >= middle_length:
+    if len(password) >= check_list['middle_length']:
         password_strength += 1
 
-    if has_lowercase and has_uppercase:
+    if check_list['has_lowercase'] and check_list['has_uppercase']:
         password_strength += 1
 
-    if has_numbers:
+    if check_list['has_numbers']:
         password_strength += 1
 
-    if has_symbols:
+    if check_list['has_symbols']:
         password_strength += 2
 
-    if not has_date:
+    if not check_list['has_date']:
         password_strength += 2
 
-    if not has_phone_number:
+    if not check_list['has_phone_number']:
         password_strength += 2
 
     return password_strength
