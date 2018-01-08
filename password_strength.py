8d3cf8d0e47e9d8a8013d809cf48d831c8510a29
Andrey (home) <ase@crimea.com>
2018-01-08 08:45:44 +0300

    refactoring func get_password_strength

--- password_strength.py
+++ password_strength.py
@@ -2,45 +2,32 @@ import getpass
 import re
 
 
-def get_check_list():
+def get_check_list(password):
     check_list = {}
+    min_length = 6
+    great_length = 10
 
-    check_list['min_length'] = 6
-    check_list['middle_length'] = 10
-    check_list['has_lowercase'] = re.search(r'[a-z]', password)
-    check_list['has_uppercase'] = re.search(r'[A-Z]', password)
-    check_list['has_numbers'] = re.search(r'\d', password)
-    check_list['has_symbols'] = re.search(r'[!@#$%^&*()_+]', password)
-    check_list['has_date'] = re.search(r'\d{2,4}[.-]\d{2}[.-]\d{2,4}', password)
-    check_list['has_phone_number'] = re.search(r'[7-8]\d{10}', password)
+    check_list['min_length'] = len(password) >= 6
+    check_list['great_length'] = len(password) >= 10
+    check_list['has_lowercase'] = bool(re.search(r'[a-z]', password))
+    check_list['has_uppercase'] = bool(re.search(r'[A-Z]', password))
+    check_list['has_numbers'] = bool(re.search(r'\d', password))
+    check_list['has_symbols'] = bool(re.search(r'[!@#$%^&*()_+]', password))
+    check_list['has_date'] = not(re.search(r'\d{2,4}[.-]\d{2}[.-]\d{2,4}', password))
+    check_list['has_phone_number'] = not(re.search(r'[7-8]\d{10}', password))
     return check_list
 
 
 def get_password_strength(password):
     password_strength = 0
 
-    check_list = get_check_list()
+    check_list = get_check_list(password)
 
-    if len(password) >= check_list['min_length']:
-        password_strength += 1
-
-    if len(password) >= check_list['middle_length']:
-        password_strength += 1
-
-    if check_list['has_lowercase'] and check_list['has_uppercase']:
-        password_strength += 1
-
-    if check_list['has_numbers']:
-        password_strength += 1
-
-    if check_list['has_symbols']:
-        password_strength += 2
-
-    if not check_list['has_date']:
-        password_strength += 2
-
-    if not check_list['has_phone_number']:
-        password_strength += 2
+    for key, value in check_list.items():
+        if value:
+            password_strength += 1
+            if key == 'great_length' or key == 'has_symbols' :
+                password_strength += 1
 
     return password_strength
 
